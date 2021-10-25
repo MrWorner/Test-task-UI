41cbc18da0722dcce71ccb6da065ba208b85aa22
HarryWorner <LegendBlake@mail.ru>
2021-10-25 16:13:50 +0600

    upd 2

--- TestTask_UI/Assets/Scripts/UI/Window/ExtentionMethods.cs
+++ TestTask_UI/Assets/Scripts/UI/Window/ExtentionMethods.cs
@@ -1,15 +1,15 @@
-﻿namespace UnityEngine.UI.Extensions
-{
-    public static class ExtentionMethods
-    {
-        public static T GetOrAddComponent<T>(this GameObject child) where T : Component
-        {
-            T result = child.GetComponent<T>();
-            if (result == null)
-            {
-                result = child.AddComponent<T>();
-            }
-            return result;
-        }
-    }
-}
+﻿//namespace UnityEngine.UI.Extensions
+//{
+//    public static class ExtentionMethods
+//    {
+//        public static T GetOrAddComponent<T>(this GameObject child) where T : Component
+//        {
+//            T result = child.GetComponent<T>();
+//            if (result == null)
+//            {
+//                result = child.AddComponent<T>();
+//            }
+//            return result;
+//        }
+//    }
+//}
